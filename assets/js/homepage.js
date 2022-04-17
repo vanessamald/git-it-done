4ea52204a643394a9378b9474f14f0e70da22d5e
Vanessa Maldonado <vanessa.rodriguez3@mavs.uta.edu>
2022-04-17 00:22:41 -0500

    updating js file

--- assets/js/homepage.js
+++ assets/js/homepage.js
@@ -1,30 +1,5 @@
 var repoContainerEl = document.querySelector("#repos-container");
 var repoSearchTerm = document.querySelector("#repo-search-term");
-
-var getUserRepos = function(user) {
-    // format the github api url
-    var apiUrl = "https://api.github.com/users/" + user + "/repos";
-
-    // make a request to the url
-    fetch(apiUrl)
-    .then(function(response) {
-        //request was successful
-        if (response.ok) {
-            response.json().then(function(data) {
-                displayRepos(data, user);
-            });
-        } else {
-            alert("Error: GitHub User Not Found");
-        }
-    })
-    .catch(function(error) {
-        // Notice this `.catch()` getting chained onto the end of the `.then()` method
-        alert("Unable to connect to GitHub");
-    });
-
-var response = fetch("https://api.github.com/users/octocat/repos");
-console.log(response);
-
 var userFormEl = document.querySelector("#user-form");
 var nameInputEl = document.querySelector("#username");
 
@@ -39,18 +14,38 @@ var formSubmitHandler = function(event) {
     } else {
         alert("Please enter a GitHub username");
     }
-    console.log(event);
+};
+
+var getUserRepos = function(user) {
+    // format the github api url
+    var apiUrl = "https://api.github.com/users/" + user + "/repos";
+
+    // make a request to the url
+    fetch(apiUrl)
+    .then(function(response) {
+        //request was successful
+        if (response.ok) {
+            console.log(response);
+            response.json().then(function(data) {
+            displayRepos(data, user);
+            });
+        } else {
+            alert('Error: ' + response.statusText);
+        }
+    })
+    .catch(function(error) {
+        // Notice this `.catch()` getting chained onto the end of the `.then()` method
+        alert("Unable to connect to GitHub");
+    });
 };
 
 var displayRepos = function(repos, searchTerm) {
     // check if api returned any repos
     if (repos.length === 0) {
         repoContainerEl.textContent = "No repositories found.";
-        reuturn;
+        return;
     }
-    console.log(repos);
-    console.log(searchTerm);
-}
+  
     // clear old content
     repoContainerEl.textContent = "";
     repoSearchTerm.textContent= searchTerm;
