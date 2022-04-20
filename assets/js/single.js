5890da4d72af83b29319d5cafc95b65216151a99
Vanessa Maldonado <vanessa.rodriguez3@mavs.uta.edu>
2022-04-19 21:33:54 -0500

    cleaning up single.jsfile

--- assets/js/single.js
+++ assets/js/single.js
@@ -2,6 +2,22 @@ var issueContainerEl = document.querySelector("#issues-container");
 var limitWarningEl = document.querySelector("#limit-warning");
 var repoNameEl = document.querySelector("#repo-name");
 
+var getRepoName =function() {
+    // grab repo name from url query string
+    var queryString = document.location.search;
+    var repoName = queryString.split("=")[1];
+
+    if(repoName) { 
+        // display repo name on the page
+        repoNameEl.textContent = repoName;
+        
+        getRepoIssues(repoName);
+} else { 
+    // if no repo was given, redirect to the homepage
+    document.location.replace("./index.html");
+}
+};
+
 var getRepoIssues = function(repo) {
     console.log(repo);
     var apiUrl = "https://api.github.com/repos/" + repo + "/issues?direction=asc";
@@ -27,21 +43,7 @@ var getRepoIssues = function(repo) {
     });
 };
 
-var getRepoName =function() {
-    // grab repo name from url query string
-    var queryString = document.location.search;
-    var repoName = queryString.split("=")[1];
 
-    if(repoName) { 
-        // display repo name on the page
-        repoNameEl.textContent = repoName;
-        
-        getRepoIssues(repoName);
-} else { 
-    // if no repo was given, redirect to the homepage
-    document.location.replace("./index.html");
-}
-};
 var displayIssues = function(issues) {
     if (issues.length === 0) {
         issueContainerEl.textContent = "This repo has no open issues!";
